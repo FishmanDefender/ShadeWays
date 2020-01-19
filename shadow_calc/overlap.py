00349eee8f26e6a3eb5c41552f326f7f300422aa
Michael Schott <mlschott343@gmail.com>
2020-01-19 00:05:59 -0700

    Starting overlap.py

---
+++ shadow_calc/overlap.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import numpy as np
+
+def overlap(shadowlist, path_coords):
+    '''
+    Overlap calculator to get the percent shadow.
+    '''
+
+    path_lat, path_long = [x[0] for x in path_coords], [x[1] for x in path_coords]
+    path_bounds = (min(path_lat), min(path_long), max(path_lat), max(path_long))
+    delta_lat, delta_long = (path_bounds[2]-path_bounds[0]), (path_bounds[3]-path_bounds[1])
+
+    grid_spacing = max(delta_lat,delta_long)/1000
+    grid_coords = list(zip(list(np.linspace(0,)),list()))
